6decbc6fd939088ed71fb8a14e6e776434f0e8cc
Mahat <oraemoroz@gmail.com>
2022-05-20 17:43:11 +0300

    make

--- platforma/settings.py
+++ platforma/settings.py
@@ -132,3 +132,4 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
