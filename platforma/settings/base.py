f4234d68fde2550c6410a2b8ab929dd8bbdc8b0b
Mahat <oraemoroz@gmail.com>
2022-06-01 12:46:26 +0300

    added install app

--- platforma/settings/base.py
+++ platforma/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'platforma',
     'universystem',
     'ckeditor',
     'ckeditor_uploader',
